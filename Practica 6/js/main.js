9b5f9888c5be2dafad42bed753ea722b954d3afc
ArtherusE <np.torres@hotmail.com>
2020-10-14 14:43:48 -0500

    a

--- Practica 6/js/main.js
+++ Practica 6/js/main.js
@@ -1,18 +1,18 @@
-var blogeo = document.getElementById('sigin');
-blogeo.addEventListener('click', function () {
-    var e = document.getElementById('email').value);
-var p = document.getElementById('password').value);
-constant parametros = new URLSearchParams();
-parametros.append('ParaEmail', e);
-parametros.append('ParaPass', p);
-axios.get('http://localhost/Adios')
-    .then(function (response) {
-        console.log(response)
-        console.log('contenido: ' + response.data)
-        console.log('estatus: ' + response.status)
-        document.getElementById('titulo').innerHTML = response.data
+var blogeo = document.getElementById("signin");
+blogeo.addEventListener("click", function() {
+    var pass = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    const parametros = new URLSearchParams();
+    parametros.append("PrmEmail", email);
+    parametros.append("PrmPassword", pass);
+    axios.get("http://192.168.0.20")
+    .then(function(response) {
+        console.log(response);
+        console.log("contenido: " + response.data);
+        console.log("status: " + response.status);
+        document.getElementById("titulo").innerHTML = response.data;
     })
-    .catch(function (error) {
+    .catch(function(error) {
         console.log(error)
     })
 })
